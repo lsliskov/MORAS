2badf234188519c4c0ccc1f6971e99457c7aa332
lsliskov <149537033+lsliskov@users.noreply.github.com>
2023-12-12 00:02:57 +0100

    Add files via upload

---
+++ zadaća3/zadatak3a.asm
@@ -0,0 +1,65 @@
+@0
+D = M
+
+@rbaza
+M = D
+
+@temp
+M = D
+
+@rez
+M = 0
+
+@1
+D = M
+
+@rexp
+M = D
+
+@1
+M = M - 1 
+
+$WHILE(1)
+
+$WHILE(0)
+
+$SUM(temp, rez, rez)
+
+@0
+M = M - 1
+
+$END
+
+@rez
+D = M
+M = 0
+
+@temp
+M = D
+
+@rbaza
+D = M
+
+@0
+M = D
+
+@1
+M = M - 1
+
+$END
+
+@rexp
+D = M
+
+@1
+M = D
+
+@temp
+D = M
+
+@2
+M = D
+
+(END)
+@END
+0;JMP
